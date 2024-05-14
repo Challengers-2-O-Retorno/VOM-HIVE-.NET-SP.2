7dd1f0046e68452e2dd28d71b4d4713538489f8b
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-14 20:02:29 -0300

    Criado nova pasta e DbContext para o modelo do Banco

---
+++ Sp2/Persistence/OracleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sp2.Models;
+
+namespace Sp2.Persistence
+{
+    public class OracleDbContext : DbContext
+    {
+        public DbSet<Campaign> Campaign { get; set; }
+
+        public DbSet<Payhist> Payhist { get; set; }
+
+        public DbSet<Paymethod> Paymethod { get; set; }
+        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
