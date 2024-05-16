ec78572b1e638f539c2d5cf10071d2ecc43b02d7
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-16 20:57:44 -0300

    Criado repository para company

---
+++ Sp2/Repositories/CompanyRepository.cs
@@ -0,0 +1,23 @@
+﻿using Sp2.Models;
+using Sp2.Persistence;
+
+namespace Sp2.Repositories
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        private readonly OracleDbContext _bancoContext;
+
+        public CompanyRepository(OracleDbContext oracleDbContext)
+        {
+             _bancoContext = oracleDbContext;
+        }
+
+        public CompanyModel Adicionar(CompanyModel company)
+        {
+            //Gravar no banco de dados
+            _bancoContext.Company.Add(company);
+            _bancoContext.SaveChanges();
+            return company;
+        }
+    }
+}
---
+++ Sp2/Repositories/ICompanyRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sp2.Models;
+
+namespace Sp2.Repositories
+{
+    public interface ICompanyRepository
+    {
+        CompanyModel Adicionar(CompanyModel company);
+    }
+}
