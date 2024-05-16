99ae0c473788dc0d5a0f3ad6036401c1c0bb194c
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-16 20:57:34 -0300

    Corrigido o nome das models

--- Sp2/Persistence/OracleDbContext.cs
+++ Sp2/Persistence/OracleDbContext.cs
@@ -5,11 +5,13 @@ namespace Sp2.Persistence
 {
     public class OracleDbContext : DbContext
     {
-        public DbSet<Campaign> Campaign { get; set; }
+        public DbSet<CampaignModel> Campaign { get; set; }
 
-        public DbSet<Payhist> Payhist { get; set; }
+        public DbSet<CompanyModel> Company { get; set; }
 
-        public DbSet<Paymethod> Paymethod { get; set; }
+        public DbSet<PayhistModel> Payhist { get; set; }
+
+        public DbSet<PaymethodModel> Paymethod { get; set; }
         public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options)
         {
             
