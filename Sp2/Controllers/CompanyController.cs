e0d770a2f9970b5551629b42466a09a19d0294c3
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-16 20:57:01 -0300

    Criaco o controller da company

---
+++ Sp2/Controllers/CompanyController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sp2.Models;
+using Sp2.Repositories;
+
+namespace Sp2.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly ICompanyRepository _companyRepository;
+
+        public CompanyController(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(CompanyModel company)
+        {
+            _companyRepository.Adicionar(company);
+            return RedirectToAction("Index");
+        }
+    }
+}
