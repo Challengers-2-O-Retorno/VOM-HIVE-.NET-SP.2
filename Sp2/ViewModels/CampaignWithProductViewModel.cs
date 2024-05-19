425c9f332d604c0d999a2644376ab86d88a7defb
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-19 14:34:11 -0300

    .

---
+++ Sp2/ViewModels/CampaignWithProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Sp2.Models;
+
+namespace Sp2.ViewModels
+{
+    public class CampaignWithProductViewModel
+    {
+        public CampaignModel Campaign { get; set; }
+        public ProductModel Product { get; set; }
+    }
+}
