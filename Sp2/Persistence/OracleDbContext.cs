c924e4e144574c21e46c19952c0bdc97f1870d15
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-19 14:33:50 -0300

    Configurando DbContext para delete dos itens

--- Sp2/Persistence/OracleDbContext.cs
+++ Sp2/Persistence/OracleDbContext.cs
@@ -11,14 +11,32 @@ namespace Sp2.Persistence
 
         public DbSet<CompanyModel> Company { get; set; }
 
-        public DbSet<PayhistModel> Payhist { get; set; }
-
-        public DbSet<PaymethodModel> Paymethod { get; set; }
 
         public DbSet<ProfileuserModel> ProfileUser { get; set; }
         public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options)
         {
-            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CompanyModel>()
+                .HasMany(c => c.Campaigns)
+                .WithOne(ca => ca.Company)
+                .HasForeignKey(ca => ca.id_company)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CompanyModel>()
+                .HasMany(c => c.Profile_users)
+                .WithOne(pu => pu.Company)
+                .HasForeignKey(pu => pu.id_company)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<ProductModel>()
+                .HasMany(p => p.Campaigns)
+                .WithOne(ca => ca.Product)
+                .HasForeignKey(ca => ca.id_product)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
